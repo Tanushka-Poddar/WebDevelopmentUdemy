8512aabd7f5f557a7b42d1549f829ead14bedc7e
Tanushka Poddar <tanushkapoddar01@gmail.com>
2025-12-10 19:42:54 +0530

    animations added

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,22 +1,26 @@
-var total = document.querySelectorAll(".drum").length;
+//detects button presses on the drum kit website
+var total = document.querySelectorAll(".drum").length;    //gets the total number of buttons with class drum
 
 for(var i = 0 ; i< total ; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",onClick);
+    document.querySelectorAll(".drum")[i].addEventListener("click",onClick);  //"click" is the TYPE OF EVENT we are listening for
     
     function onClick(){
-        var buttonClicked = this.innerHTML;
+        var buttonClicked = this.innerHTML;  //the innerHTML of the button that was clicked is stored in buttonClicked variable like w,a,s,d,j,k,l ands this refers to the button that was clicked when the current for loop runs 
 
-        keyPresses(buttonClicked);
+        keyPresses(buttonClicked);  
+        buttonAnimation(buttonClicked);  //calls the function and the entire switch statement is executed based on the button clicked
 
     }
 }
 
-document.addEventListener("keydown", function(event){
-    keyPresses(event.key);
+//detects keyboard presses
 
+document.addEventListener("keydown", function(event){  //the event paarmeter listens to the event that triggered the function and contains the information on the key that was pressed
+    keyPresses(event.key);  //event.key gives the value of the key that was pressed
+    buttonAnimation(event.key);
     });
 
-    function keyPresses(key){
+    function keyPresses(key){     //key is taken as the function parameter here since it determines what sound will be produced 
 
         switch(key){
             case "w":
@@ -59,6 +63,18 @@ document.addEventListener("keydown", function(event){
 
     }
 }
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);  //selects the button with the class of the key that was pressed or clicked
+
+    activeButton.classList.add("pressed");  //adds the class "pressed" to the button that was clicked or pressed
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100 );
+    
+    }
+
 
     
         
